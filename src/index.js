42d86cad80315a44cb9efdc19adfc54e80848561
Israa Tahon <esra.tahon@gmail.com>
2020-04-10 19:07:32 +0200

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export function fetchMovies() {}
+import { require } from "movies";
+import {
+    fetchWithTimeout
+}
+from "./services";
+
+
+
+export function fetchMovies() {
+    const resolveFunction = () => movies;
+   
+}
